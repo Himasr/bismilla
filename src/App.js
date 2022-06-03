232969c1d87efc8b0a71454a78aa48231ee5371d
Himasr <srihima.100@gmail.com>
2022-06-03 12:49:41 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,57 @@
+//import logo from './logo.svg';
+import './App.css';
+import React from 'react';
+
+
+function App() {
+  return (
+    <div className="round1">
+    <div className='over'>
+    <div className='a'>
+      <h1>Bismilla Enterprises</h1>
+      
+      <div class="dropdown">
+  <button class="dropbtn">MENU</button>
+  <div class="dropdown-content">
+    <a href="#">Buy</a>
+    <a href="#">Sell</a>
+    <a href="#">Contact US</a>
+    <a href="#">Chat</a>
+    
+  </div></div>
+  
+  <button class="dropbtn1">Login</button>
+
+<div className='left'>
+  <h2>Selling your old car has never been safer or faster.Get an offer for your used car now!.</h2>
+  <img src="https://images.pexels.com/photos/5432071/pexels-photo-5432071.jpeg" ></img>
+  
+  </div>
+  <div className='left1'>
+  <h2>Bismilla Enterprises is one of the best Metal trading company.We have been dealing with steel,aluminum,and other types of metals since 1987.</h2>
+  <img src="https://images.pexels.com/photos/5432071/pexels-photo-5432071.jpeg" ></img>
+  
+  </div>
+ 
+<div class="right">
+  <h2>Buy best used car parts online in india.Offer 100% verified secound-hand parts at best price with easy finance and transfer.</h2>
+  <img src="https://images.pexels.com/photos/164634/pexels-photo-164634.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" ></img>
+  
+</div>
+
+<div className='left1'>
+<h4>One of our Happy ELV Owners </h4>
+<img src="https://images.pexels.com/photos/4320869/pexels-photo-4320869.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" ></img>
+</div>
+<h3>DO YOU WANT TO DISPOSE YOUR OLD VEHICAL(ELV)? <button class="dropbtn2">Click Me</button> </h3>
+</div>
+ </div>
+   
+ </div>
+  
+
+  );
+}
+
+export default App;
+
